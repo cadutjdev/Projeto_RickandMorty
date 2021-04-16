5cb1cc1a457f53a03e800626895d8cde9b4db09a
cadutjdev <cadutjdev@gmail.com>
2021-04-16 20:49:36 -0300

    Stop no projeto

--- arquivo.js
+++ arquivo.js
@@ -1,8 +1,16 @@
 'use strict';
 
-const pesquisarNome = () => {
-    const nome = document.getElementById('nome').value;
-    const url = `https://rickandmortyapi.com/api/character`
+const pesquisarNome = async() => {
+    const nome = document.getElementById('campoNome').value;
+    const url = `https://rickandmortyapi.com/api/character/?name=rick&status=alive${nome}`;
+    const dados = await fetch(url);
+    const status = await dados.query();
+    const genero = await dados.query();
+}
 }
 
-document.getElementById('nome').addEventListener('focusout', pesquisarNome);
+document.getElementById('campoNome').addEventListener('focusout', pesquisarNome);
+
+document.getElementById('campoNome').addEventListener('focusout', pesquisarNome);
+
+document.getElementById('campoNome').addEventListener('focusout', pesquisarNome);
